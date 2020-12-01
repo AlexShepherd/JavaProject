47a763473f94c9b296e406e289be15a39f2fc147
rhern4892 <49795768+rhern4892@users.noreply.github.com>
2020-11-30 18:43:44 -0600

    Update and rename HealthKit.java to FirstAidKit.java

--- src/FirstAidKit.java
+++ src/FirstAidKit.java
@@ -1,50 +1,50 @@
 // ---------------------------------------------------------------------------
-// File name: Helmet.java
+// File name: FirstAidKit.java
 // Project name: Game Project
 // ---------------------------------------------------------------------------
 // Creator’s name and email: Roberto Hernandez, hernandezr@etsu.edu
 // Course-Section: CSCI-1260-900
 // Creation Date: 11-13-2020
-// Date of Last Modification: 11-15-2020
+// Date of Last Modification: 11-30-2020
 // ---------------------------------------------------------------------------
 
 /**
- * Class Name: HealthKit<br>
- * Class Purpose: HealthKit extends Consumable<br>
+ * Class Name: FirstAidKit<br>
+ * Class Purpose: FirstAidKit extends Consumable<br>
  *
  * <hr>
  * Date created: 11-13-2020
  * Date last modified: 11-15-2020
  */
-public class HealthKit extends Consumable {
+public class FirstAidKit extends Consumable {
 
     /**
-     * Method Name: HealthKit<br>
+     * Method Name: FirstAidKit<br>
      * Method Purpose: parameterized constructor<br>
      *
      * <hr>
      * Date created: 11-13-2020
      * Date last modified: 11-15-2020
      */
-    public HealthKit(String name, int price, double durability) {
+    public FirstAidKit(String name, int price, double durability) {
         super(name, price, durability);
     }
 
     /**
-     * Method Name: HealthKit<br>
-     * Method Purpose: super constructor<br>
+     * Method Name: FirstAidKit<br>
+     * Method Purpose: constructor<br>
      *
      * <hr>
      * Date created: 11-13-2020
      * Date last modified: 11-15-2020
      */
-    public HealthKit() {
-        this("Health Kit", 50, 1);
+    public FirstAidKit() {
+        this("First Aid Kit", 50, 1);
     }
 
     /**
      * Method Name: use<br>
-     * Method Purpose: uses HealthKit<br>
+     * Method Purpose: uses FirstAidKit<br>
      *
      * <hr>
      * Date created: 11-13-2020
