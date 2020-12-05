449292fec3cac31cd56b51cfc35cf8dd9ee67657
rhern4892 <49795768+rhern4892@users.noreply.github.com>
2020-12-05 15:32:32 -0600

    added radiation classes

--- src/IHitable.java
+++ src/IHitable.java
@@ -5,7 +5,7 @@
 // Creator’s name and email: Roberto Hernandez, hernandezr@etsu.edu
 // Course-Section: CSCI-1260-900
 // Creation Date: 11-4-2020
-// Date of Last Modification: 11-4-2020
+// Date of Last Modification: 12-5-2020
 // ---------------------------------------------------------------------------
 
 /**
@@ -14,14 +14,19 @@
  *
  * <hr>
  * Date created: 11-4-2020
- * Date last modified: 11-4-2020
+ * Date last modified: 12-5-2020
  * @author Roberto Hernandez
  */
 public interface IHitable
 {
     public int getMaxHealth();
     public int getCurrentHealth();
+    public int getMaxRadiation();
+    public int getRadiationStatus();
     public boolean isHitableDestroyed();
-    public int takeDamage(int dmg, int fire, int ice);
+    public boolean isHitableRadiated();
+    public int takeDamage(int dmg, int fire, int acid);
+    public int takeRadiation(int acid);
     public int heal(int amt);
+    public int radAway(int amt);
 }
