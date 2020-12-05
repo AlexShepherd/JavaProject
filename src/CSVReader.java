50946185301275a81cccb97dae17d6c834a75e75
AlexShepherd <ashepherd2821@gmail.com>
2020-12-04 20:55:43 -0500

    add filereader, gui

---
+++ src/CSVReader.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+    String file;
+    String row;
+    int roomDataRows = 3;
+    int roomDataColumns = 2;
+    int i;
+
+    public CSVReader(String filePath) {
+        file = filePath;
+    }
+
+    public String[][] readData() throws IOException {
+        BufferedReader csvReader = new BufferedReader(new FileReader(file));
+        String[][] allRoomData = new String[roomDataRows][roomDataColumns];
+        i = 0;
+        while ((row = csvReader.readLine()) != null) {
+            String[] roomData = row.split(",");
+            allRoomData[i] = roomData;
+            i++;
+        }
+        csvReader.close();
+        return allRoomData;
+    }
+}
--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
@@ -16,18 +17,30 @@ public class GUI extends Application {
         Button moveDownButton = new Button("↓");
         Button moveLeftButton = new Button("←");
         Button moveRightButton = new Button("→");
-        Button attackButton = new Button();
-        Button idkButton = new Button();
+        Button attackButton = new Button("ATTACK");
+        Button idkButton = new Button("IDK YET");
+        Text text = new Text("This is a JavaFX text.\nLine 2");
+
+
 
         GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.BOTTOM_CENTER);
 
         gridPane.add(moveUpButton, 5, 5, 1, 1);
         gridPane.add(moveDownButton, 5, 6, 1, 1);
         gridPane.add(moveLeftButton, 4, 6, 1, 1);
         gridPane.add(moveRightButton, 6, 6, 1, 1);
+        gridPane.add(attackButton, 1, 6, 1, 1);
+        gridPane.add(idkButton, 10, 6, 1, 1);
+        gridPane.setGridLinesVisible(true);
+        gridPane.setHgap(20);
+        gridPane.setVgap(5);
+        gridPane.add(text, 5, 4, 1, 1);
 
         Scene scene = new Scene(gridPane, 512, 512);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
     }
 }
